780dafcf51f9a98485ae77697fe71f8fe6f1d72a
Eugene Prikazchikov <eprikazc@gmail.com>
2018-11-15 11:46:12 +0300

    Add scripts for creating table and importing data

---
+++ scripts/create_table.py
@@ -0,0 +1,32 @@
+import argparse
+
+from psycopg2 import connect, sql
+
+
+def create_table(table_name, metric_column_name, connection_string=''):
+    connection = connect(connection_string)
+    cursor = connection.cursor()
+    stmt = sql.SQL(
+        '''
+        CREATE TABLE {} (
+            geo_level varchar,
+            geo_code varchar,
+            {} varchar,
+            total varchar,
+            geo_version varchar
+        )''') \
+        .format(
+            sql.Identifier(table_name),
+            sql.Identifier(metric_column_name))
+    cursor.execute(stmt)
+    connection.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("table_name")
+    parser.add_argument("metric_column_name")
+    parser.add_argument('-c', '--connection-string', default='')
+    args = parser.parse_args()
+    create_table(
+        args.table_name, args.metric_column_name, args.connection_string)
---
+++ scripts/csv_import.py
@@ -0,0 +1,47 @@
+import argparse
+import csv
+
+from psycopg2 import connect, sql
+
+
+def csv_to_table(file_path, table_name, metric_column_name, skip_fields, geo_version, connection_string):
+    skip_fields = ['', 'geo_level', 'geo_code'] + skip_fields
+    connection = connect(connection_string)
+    cursor = connection.cursor()
+    stmt = sql.SQL("""
+        INSERT INTO {}
+            (geo_version, geo_level, geo_code, {}, total)
+            VALUES(%s, %s, %s, %s, %s)
+        """) \
+        .format(
+            sql.Identifier(table_name),
+            sql.Identifier(metric_column_name),
+        )
+    with open(file_path) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            for column, value in row.items():
+                if column in skip_fields:
+                    continue
+                cursor.execute(
+                    stmt,
+                    (geo_version, row['geo_level'], row['geo_code'], column, value))
+    connection.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path")
+    parser.add_argument("table_name")
+    parser.add_argument("metric_column_name")
+    parser.add_argument(
+        '-s', '--skip', action='append',
+        help='Column that should be ignored. This argument can be repeated.')
+    parser.add_argument(
+        '-g', '--geo-version', default='2011',
+        help='Geo version, by default=2011')
+    parser.add_argument('-c', '--connection-string', default='')
+    args = parser.parse_args()
+    csv_to_table(
+        args.file_path, args.table_name, args.metric_column_name,
+        args.skip, args.geo_version, args.connection_string)
